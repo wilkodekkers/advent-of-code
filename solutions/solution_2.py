546204f910663b9f0144e4b446307a7bf3311584
Wilko Dekkers <wilko.dekkers@gmail.com>
2022-12-01 19:09:31 +0100

    Prepapre for day 2

--- solutions/solution_2.py
+++ solutions/solution_2.py
@@ -1,27 +1,13 @@
 from dataclasses import dataclass
 
-from solutions.solution import Solution
+from solutions import Solution
 
 
 @dataclass
-class Solution1(Solution):
-    elf_list = []
-
-    def __init__(self, path_to_input: str):
-        super().__init__(path_to_input)
-        self.fill_elf_list()
+class Solution2(Solution):
 
     def part1(self) -> int:
-        return max(self.elf_list)
+        return -1
 
     def part2(self) -> int:
-        return sum(sorted(self.elf_list, reverse=True)[0:3])
-
-    def fill_elf_list(self):
-        elf = 0
-        for calorie in self.lines:
-            if calorie:
-                elf += int(calorie)
-            else:
-                self.elf_list.append(elf)
-                elf = 0
+        return -1
