fae1975a88813d9c904e0c92ccd458be8b3f0287
Wilko Dekkers <wilko.dekkers@gmail.com>
2022-12-02 13:08:51 +0100

    Optimize solution by looking at other implementations

--- solutions/solution_2.py
+++ solutions/solution_2.py
@@ -2,51 +2,23 @@ from solutions import Solution
 
 
 class Solution2(Solution):
-    ACTION_SCORE = {
-        'A': 1,
-        'B': 2,
-        'C': 3,
-        'X': 1,
-        'Y': 2,
-        'Z': 3
-    }
 
     def part1(self) -> int:
         score = 0
         for line in self.lines:
-            opponent, me = line.split(" ")
-            base = self.ACTION_SCORE[me]
-            result = (self.ACTION_SCORE[opponent], self.ACTION_SCORE[me])
-            bonus_points = 0
-            match result:
-                case (1, 2) | (2, 3) | (3, 1):
-                    bonus_points = 6
-                case (a, b):
-                    bonus_points = 3 if (a == b) else 0
-            score += base + bonus_points
+            op, me = line.split()
+            score += {'X': 1, 'Y': 2, 'Z': 3}[me]
+            score += {('A', 'X'): 3, ('A', 'Y'): 6, ('A', 'Z'): 0,
+                      ('B', 'X'): 0, ('B', 'Y'): 3, ('B', 'Z'): 6,
+                      ('C', 'X'): 6, ('C', 'Y'): 0, ('C', 'Z'): 3}[(op, me)]
         return score
 
     def part2(self) -> int:
         score = 0
         for line in self.lines:
-            opponent, me = line.split(" ")
-            opponent = self.ACTION_SCORE[opponent]
-            me = self.ACTION_SCORE[me]
-            match me:
-                case 1:
-                    me = 3 if (opponent - 1 < 1) else opponent - 1
-                case 2:
-                    me = opponent
-                case 3:
-                    me = 1 if (opponent + 1 > 3) else opponent + 1
-                case _:
-                    me = 0
-            result = (opponent, me)
-            bonus_points = 0
-            match result:
-                case (1, 2) | (2, 3) | (3, 1):
-                    bonus_points = 6
-                case (a, b):
-                    bonus_points = 3 if (a == b) else 0
-            score += me + bonus_points
+            op, me = line.split()
+            score += {'X': 0, 'Y': 3, 'Z': 6}[me]
+            score += {('A', 'X'): 3, ('A', 'Y'): 1, ('A', 'Z'): 2,
+                      ('B', 'X'): 1, ('B', 'Y'): 2, ('B', 'Z'): 3,
+                      ('C', 'X'): 2, ('C', 'Y'): 3, ('C', 'Z'): 1}[(op, me)]
         return score
