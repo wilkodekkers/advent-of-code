a17a151fd0126288f85aec793f41301986de97b0
Wilko Dekkers <wilko.dekkers@gmail.com>
2022-12-02 12:50:46 +0100

    Optimized code by looking at other solutions

--- solutions/solution_2.py
+++ solutions/solution_2.py
@@ -2,18 +2,6 @@ from solutions import Solution
 
 
 class Solution2(Solution):
-    LOSING_ACTION = {
-        'A': 'Z',
-        'B': 'X',
-        'C': 'Y'
-    }
-
-    ACTION = {
-        'A': 'Y',
-        'B': 'Z',
-        'C': 'X'
-    }
-
     ACTION_SCORE = {
         'A': 1,
         'B': 2,
@@ -27,58 +15,38 @@ class Solution2(Solution):
         score = 0
         for line in self.lines:
             opponent, me = line.split(" ")
-            score += self.calculate_score(opponent, me)
+            base = self.ACTION_SCORE[me]
+            result = (self.ACTION_SCORE[opponent], self.ACTION_SCORE[me])
+            bonus_points = 0
+            match result:
+                case (1, 2) | (2, 3) | (3, 1):
+                    bonus_points = 6
+                case (a, b):
+                    bonus_points = 3 if (a == b) else 0
+            score += base + bonus_points
         return score
 
     def part2(self) -> int:
         score = 0
         for line in self.lines:
             opponent, me = line.split(" ")
-            score += self.calculate_action(opponent, me)
+            opponent = self.ACTION_SCORE[opponent]
+            me = self.ACTION_SCORE[me]
+            match me:
+                case 1:
+                    me = 3 if (opponent - 1 < 1) else opponent - 1
+                case 2:
+                    me = opponent
+                case 3:
+                    me = 1 if (opponent + 1 > 3) else opponent + 1
+                case _:
+                    me = 0
+            result = (opponent, me)
+            bonus_points = 0
+            match result:
+                case (1, 2) | (2, 3) | (3, 1):
+                    bonus_points = 6
+                case (a, b):
+                    bonus_points = 3 if (a == b) else 0
+            score += me + bonus_points
         return score
-
-    def rock_strategy(self, me: str) -> int:
-        match me:
-            case 'X':  # tie
-                return 1 + 3
-            case 'Y':  # win
-                return 2 + 6
-            case 'Z':  # lose
-                return 3 + 0
-
-    def paper_strategy(self, me: str) -> int:
-        match me:
-            case 'X':  # lose
-                return 1 + 0
-            case 'Y':  # tie
-                return 2 + 3
-            case 'Z':  # win
-                return 3 + 6
-
-    def scissors_strategy(self, me: str) -> int:
-        match me:
-            case 'X':  # win
-                return 1 + 6
-            case 'Y':  # lose
-                return 2 + 0
-            case 'Z':  # tie
-                return 3 + 3
-
-    def calculate_score(self, opponent: str, me: str) -> int:
-        match opponent:
-            case 'A':
-                return self.rock_strategy(me)
-            case 'B':
-                return self.paper_strategy(me)
-            case 'C':
-                return self.scissors_strategy(me)
-            case _:
-                return 0
-
-    def calculate_action(self, opponent: str, me: str) -> int:
-        if me == 'Y':
-            return self.ACTION_SCORE[opponent] + 3
-        elif me == 'X':
-            return self.ACTION_SCORE[self.LOSING_ACTION[opponent]]
-        else:
-            return self.ACTION_SCORE[self.ACTION[opponent]] + 6
