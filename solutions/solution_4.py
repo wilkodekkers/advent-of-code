cb7b96a103aa3f2350ae8da82e47b13b90aeb43b
Wilko Dekkers <wilko.dekkers@gmail.com>
2022-12-04 13:28:21 +0100

    Minimize solution with help

--- solutions/solution_4.py
+++ solutions/solution_4.py
@@ -5,43 +5,15 @@ class Solution4(Solution):
 
     def __init__(self):
         super().__init__()
-        self.elf_pairs = []
+        self.data = []
 
     def load(self, path_to_input_file: str) -> "Solution":
         super().load(path_to_input_file)
-        self.elf_pairs = list(map(self.parse_elves, self.lines))
+        self.data = [[int(n) for elf in line.split(',') for n in elf.split('-')] for line in self.lines]
         return self
 
-    @staticmethod
-    def parse_elves(line: str) -> list:
-        return [list(map(lambda val: int(val), x.split('-'))) for x in line.split(',')]
-
-    @staticmethod
-    def calc_dif(line) -> list[bool, bool, bool, bool]:
-        return [
-            line[1][0] - line[0][0] >= 0,
-            line[1][0] - line[0][1] > 0,
-            line[1][1] - line[0][0] >= 0,
-            line[1][1] - line[0][1] > 0
-        ]
-
-    @staticmethod
-    def sub_in_pair(pair) -> bool:
-        if pair[0][0] <= pair[1][0] and pair[0][1] >= pair[1][1]:
-            return True
-        elif pair[0][0] >= pair[1][0] and pair[0][1] <= pair[1][1]:
-            return True
-        return False
-
     def part1(self) -> int:
-        return sum(map(self.sub_in_pair, self.elf_pairs))
+        return sum((a >= c and b <= d) or (c >= a and d <= b) for a, b, c, d in self.data)
 
     def part2(self) -> int:
-        dif_list = map(self.calc_dif, self.elf_pairs)
-        solution = [
-            [True, False, True, True],
-            [True, False, True, False],
-            [False, False, True, True],
-            [False, False, True, False],
-        ]
-        return sum(x in solution for x in dif_list)
+        return sum(b >= c and d >= a for a, b, c, d in self.data)
